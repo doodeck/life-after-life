d000728ae28b8d3537950e78183f3f55cdf50f20
Kris Dudkiewicz <doodeck@users.noreply.github.com>
2015-05-26 07:47:52 +0200

    As of AWS CLI 1.7 upload-function option has been discontinued

--- upload-function.sh
+++ upload-function.sh
@@ -1,3 +1,13 @@
 #
 
-zip -r tmp/index.zip *.js modules/ node_modules/ ; aws --profile lambda lambda upload-function --region eu-west-1 --function-name life-after-life --function-zip tmp/index.zip  --role 'arn:aws:iam::915133436062:role/lal_lambda_exec_role' --mode event --handler life-after-life.handler --runtime nodejs --timeout 15
+mkdir -p tmp
+zip -r tmp/index.zip *.js modules/ node_modules/
+# As of CLI 1.7 upload-function option has been discontinued. Use update-function-code instead
+# aws --profile lambda lambda upload-function --region eu-west-1 \
+#  --function-name life-after-life \
+#  --function-zip tmp/index.zip  \
+#  --role 'arn:aws:iam::915133436062:role/lal_lambda_exec_role' --mode event \
+#  --handler life-after-life.handler --runtime nodejs --timeout 15
+aws --profile lambda lambda update-function-code --region eu-west-1 \
+  --function-name life-after-life \
+  --zip-file fileb://tmp/index.zip
